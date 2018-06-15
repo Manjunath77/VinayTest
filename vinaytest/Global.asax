04a405a8108637ba7b8a8667a5eda113a5635821
vyanamadala <vyanamadala@bodhtree.com>
2018-06-15 15:41:25 +0530

    testing

---
+++ vinaytest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vinaytest.Global" Language="C#" %>
---
+++ vinaytest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vinaytest.Startup))]
+namespace vinaytest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
